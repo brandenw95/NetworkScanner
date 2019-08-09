df73a49b1524cc8df25fb7530bd0c2bfa38ee774
Branden <the_digital_one@live.ca>
2019-08-09 00:14:17 -0700

    Create ping_network.py

---
+++ ping_network.py
@@ -0,0 +1,62 @@
+import socket 
+from socket import *
+import os
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+import multiprocessing
+import subprocess
+from multiprocessing import Pool
+
+
+TIMEOUT = 5 
+CONCURRENCY = 100  #pings in parallel
+
+def format_ips(hostnames, ip_list):
+
+    for ip, host in zip(ip_list, hostnames):
+        print(ip + " is online " + "(" + host + ")" )
+    
+
+def get_hostname(ip_list):
+
+    hostnames = []
+    
+    for ip in ip_list:
+        host = getfqdn(ip)
+        hostnames.append(host)
+
+    return hostnames
+
+def ping(ip):
+
+    ret = subprocess.call(['ping ', '-w ', str(TIMEOUT), '-n ', '1 ', ip], stdout=subprocess.DEVNULL)
+    if ret == 0:
+        #print(ip + " is online")
+        return ip
+
+def main():
+
+    internal_ip = input("Enter the subnet: ")
+    internal_ip = internal_ip[:10]
+    ips = (internal_ip + '%d' % i for i in range(1, 255))
+
+    ip_list_main = []
+    online_ip_list = []
+
+    p = Pool()
+    result = p.map(ping, ips)
+
+    p.close()
+    p.join()
+
+    for val in result:
+        if val != None:
+            online_ip_list.append(val)
+
+    hostname_list = get_hostname(online_ip_list)
+    format_ips(hostname_list, online_ip_list)
+
+
+
+if __name__ == "__main__":
+    main()
